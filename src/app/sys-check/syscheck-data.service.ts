35925e8c29d5112482a2b326a886c2cbbc96c8c6
Dan Barbulescu <danbarbulescu.main@gmail.com>
2018-12-17 14:09:56 +0100

    network check - improvements;

--- src/app/sys-check/syscheck-data.service.ts
+++ src/app/sys-check/syscheck-data.service.ts
@@ -38,34 +38,34 @@ export class SyscheckDataService {
 
     /*
 
-    <100 KB download und <50KB upload ---> unzureichend
-    100KB - 1MB download; 50KB - 1MB upload ---> langsam
-    1MB download - 10MB download; 1 MB upload bis 10MB upload; ---> ok
-    10MB - 100MB download; 10MB to 100MB upload; ---> schnell
-    100MB - 1000MB download; 100MB to 1000MB upload; ---> sehr schnell
-    1000+ download; 1000+ upload; ----> wow
+    <100 KB download und <50KB upload ---> insufficient (~ < 1Mb download; ~ < 512Kb upload)
+    100 KB - 200 KB download; 50KB - 100 KB upload; ---> slow (~ 1-2Mb download; ~ 0.5-1 Mb upload)
+    200 KB - 1MB download; 100KB - 1MB upload ---> ok (~ 2-8Mb download; <  1-8Mb upload)
+    1MB download - 10MB download; 1 MB upload - 10MB upload; ---> fast (~ 8-80Mb download; ~ 8-80Mb upload)
+    10MB - 100MB download; 10MB to 100MB upload; ---> very fast (~ 80-800 Mb download; ~80-800Mb upload)
+    100MB+ download; 100MB+ upload; ----> wow (> 800 Mb download; > 800 Mb upload)
 
     */
 
     if ((nd.downloadTest < 1024 * 100) || (nd.uploadTest < 1024 * 50)) {
-        return 'unzureichend';
+        return 'insufficient';
     } else {
-      if ((nd.downloadTest < 1024 * 1024) || (nd.uploadTest < 1024 * 1024)) {
-          return 'langsam';
+      if ((nd.downloadTest < 1024 * 200) || (nd.uploadTest < 1024 * 100)) {
+        return 'slow';
       } else {
-          if ((nd.downloadTest < 1024 * 1024 * 10) || (nd.uploadTest < 1024 * 1024 * 10)) {
-              return 'ok';
-          } else {
-              if ((nd.downloadTest < 1024 * 1024 * 100) || (nd.uploadTest < 1024 * 1024 * 100)) {
-                  return 'schnell';
-              } else {
-                  if ((nd.downloadTest < 1024 * 1024 * 1000) || (nd.uploadTest < 1024 * 1024 * 1000)) {
-                      return 'sehr schnell';
-                  } else {
-                      return 'wow';
-                  }
-              }
-          }
+        if ((nd.downloadTest < 1024 * 1024) || (nd.uploadTest < 1024 * 1024)) {
+            return 'ok';
+        } else {
+            if ((nd.downloadTest < 1024 * 1024 * 10) || (nd.uploadTest < 1024 * 1024 * 10)) {
+                return 'fast';
+            } else {
+                if ((nd.downloadTest < 1024 * 1024 * 100) || (nd.uploadTest < 1024 * 1024 * 100)) {
+                    return 'very fast';
+                } else {
+                    return 'wow';
+                }
+            }
+        }
       }
     }
   }
@@ -89,4 +89,4 @@ export interface NetworkRequestTestResult {
   'duration': number;
 }
 
-export type NetworkRating = 'N/A' | 'unzureichend' | 'langsam' | 'ok' | 'schnell' | 'sehr schnell' | 'wow';
+export type NetworkRating = 'N/A' | 'insufficient' | 'very slow' | 'slow' | 'ok' | 'fast' | 'very fast' | 'wow';
