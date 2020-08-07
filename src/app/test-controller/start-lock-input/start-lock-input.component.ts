2dacf08ba02031ed214839d9a3bca7ad12311467
paf <paf@titelfrei.de>
2020-08-07 18:32:11 +0200

    linting

--- src/app/test-controller/start-lock-input/start-lock-input.component.ts
+++ src/app/test-controller/start-lock-input/start-lock-input.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {StartLockData} from "../test-controller.interfaces";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {StartLockData} from '../test-controller.interfaces';
 
 @Component({
   templateUrl: './start-lock-input.component.html',
@@ -11,8 +11,8 @@ export class StartLockInputComponent {
   startkeyform: FormGroup;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: StartLockData) {
-
+    @Inject(MAT_DIALOG_DATA) public data: StartLockData
+  ) {
     const myFormControls = {};
     this.data.codes.forEach(c => {
       myFormControls[c.testletId] = new FormControl(c.value, [Validators.required, Validators.minLength(3)]);
