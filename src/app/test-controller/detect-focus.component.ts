d6e49a0cfd43692abd805a486816b0b50c28a1d7
paf <paf@titelfrei.de>
2020-08-13 11:18:31 +0200

    refine DetectFocusComponent

--- src/app/test-controller/detect-focus.component.ts
+++ src/app/test-controller/detect-focus.component.ts
@@ -1,53 +1,10 @@
 import {Component} from '@angular/core';
-import {FocusService} from './focus.service';
 
-
-// work in progress: read more info @ https://github.com/iqb-berlin/testcenter-frontend/issues/179
 
 @Component({
     selector: 'tc-detect-focus',
     template: '<span style="color:white"></span>'
 })
 export class DetectFocusComponent {
-    constructor(
-        private focusService: FocusService
-    ) {
-        // Set the name of the hidden property and the change event for visibility
-        let hidden, visibilityChange;
-        if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
-            hidden = 'hidden';
-            visibilityChange = 'visibilitychange';
-        } else if (typeof document['msHidden'] !== 'undefined') {
-            hidden = 'msHidden';
-            visibilityChange = 'msvisibilitychange';
-        } else if (typeof document['mozHidden'] !== 'undefined') {
-            hidden = 'mozHidden';
-            visibilityChange = 'mozHidden';
-        } else if (typeof document['webkitHidden'] !== 'undefined') {
-            hidden = 'webkitHidden';
-            visibilityChange = 'webkitvisibilitychange';
-        }
 
-        document.addEventListener(visibilityChange, (event: Event) => {
-            console.log('visibilityChange event', event, document[hidden]);
-            this.focusService.focus$.next(document[hidden] ? 'outside' : 'tab');
-        }, false);
-
-
-        window.addEventListener('blur', () => {
-            if (!document.hasFocus()) {
-                this.focusService.focus$.next('outside');
-            } else if (document.activeElement.tagName.toLowerCase() === 'iframe') { // works in chrome, not FF
-                this.focusService.focus$.next('player');
-            }
-        });
-
-        window.addEventListener('focus', () => {
-            this.focusService.focus$.next('host');
-        });
-
-        window.addEventListener('unload', () => {
-            // navigator.sendBeacon('/log', analyticsData); // TODO use sendBacon to send tell BE page was left
-        });
-    }
 }
--- src/app/test-controller/focus.service.ts
+++ src/app/test-controller/focus.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
 
+// work in progress: read more info @ https://github.com/iqb-berlin/testcenter-frontend/issues/179
 
 export type FocusTarget =
-    'tab'
+    'window'
     | 'host'
     | 'player'
     | 'outside';
@@ -12,4 +13,51 @@ export type FocusTarget =
 @Injectable()
 export class FocusService {
     public focus$: Subject<FocusTarget> = new Subject<FocusTarget>();
+
+    constructor(
+    ) {
+        window.addEventListener('blur', () => {
+            if (!document.hasFocus()) {
+                this.focus$.next('outside');
+            } else if (document.activeElement.tagName.toLowerCase() === 'iframe') {
+                // this works in chrome, not FF
+                // maybe it could be deployed for FF as well somehow
+                // BUT we still needed the player to talk about the focus with the host,
+                // since we have to detect a change from player to a completely other window
+                this.focus$.next('player');
+            }
+        });
+
+        window.addEventListener('focus', () => {
+            this.focus$.next('host');
+        });
+
+        window.addEventListener('unload', () => {
+            // navigator.sendBeacon('/log', analyticsData); // TODO use sendBacon to send tell BE page was left
+        });
+
+        // Set the name of the hidden property and the change event for visibility according to browser
+        let hidden, visibilityChange;
+        if (typeof document.hidden !== 'undefined') {
+            hidden = 'hidden';
+            visibilityChange = 'visibilitychange';
+        } else if (typeof document['msHidden'] !== 'undefined') {
+            hidden = 'msHidden';
+            visibilityChange = 'msvisibilitychange';
+        } else if (typeof document['mozHidden'] !== 'undefined') {
+            hidden = 'mozHidden';
+            visibilityChange = 'mozHidden';
+        } else if (typeof document['webkitHidden'] !== 'undefined') {
+            hidden = 'webkitHidden';
+            visibilityChange = 'webkitvisibilitychange';
+        } else {
+            console.warn('visibilityChange not supported by browser');
+            return;
+        }
+
+        document.addEventListener(visibilityChange, (event: Event) => {
+            console.log('visibilityChange event', event, document[hidden]);
+            this.focus$.next(document[hidden] ? 'outside' : 'window');
+        }, false);
+    }
 }
