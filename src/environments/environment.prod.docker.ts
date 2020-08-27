f80e97fa0b10bf9736956c206ddfa31e2106d63d
rhenck <richard.henck@tu-berlin.de>
2020-08-27 18:14:38 +0200

    Fix default backend address for docker setup

--- src/environments/environment.prod.docker.ts
+++ src/environments/environment.prod.docker.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: true,
-  testcenterUrl: 'http://localhost:9092/',
+  testcenterUrl: 'http://localhost/api/',
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
   apiVersionExpected: '5.0.1'
 };
