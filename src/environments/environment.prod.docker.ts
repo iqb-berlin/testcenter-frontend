6c299c3ec6246662fb0985ad21fafbb92b2661a4
rhenck <richard.henck@tu-berlin.de>
2020-09-02 11:34:36 +0200

    Remove docker specific environment file

--- src/environments/environment.prod.docker.ts
+++
@@ -1,9 +0,0 @@
-// ng build --prod
-
-export const environment = {
-  production: true,
-  testcenterUrl: 'http://localhost/api/',
-  appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '5.0.1',
-  veronaApiVersionSupported: '2.1.0'
-};
