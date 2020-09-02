58d18efd2b4b4fad3e3853387d37be8ef672c89a
rhenck <richard.henck@tu-berlin.de>
2020-09-02 11:35:05 +0200

    Add Verona API setting to prod environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   production: true,
   testcenterUrl: '/api/',
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '5.0.1'
+  apiVersionExpected: '5.0.1',
+  veronaApiVersionSupported: '2.1.0'
 };
