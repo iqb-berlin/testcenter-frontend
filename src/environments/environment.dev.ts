c0116fa4f4aac6ab09870bc692be732f34abfe0e
paf <paf@titelfrei.de>
2021-01-29 12:13:52 +0100

    patch apiVersionExpected

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,6 +6,6 @@ export const environment = {
   production: false,
   testcenterUrl: '/api/',
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '9.0.0',
+  apiVersionExpected: '9.1.0',
   veronaApiVersionSupported: '2.1.0'
 };
