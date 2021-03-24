cb47ff1c39b0f53bfb8d1fc6137871bfe61d5ce2
rhenck <richard.henck@iqb.hu-berlin.de>
2021-03-24 17:07:59 +0100

    Add dev environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  testcenterUrl: '/api/',
+  appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
+  apiVersionExpected: '9.1.0',
+  veronaApiVersionSupported: '2.1.0'
+};
