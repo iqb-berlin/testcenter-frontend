f5de93cd0caf19283a43ad74a590efcb9af90527
paf <paf@titelfrei.de>
2021-03-26 08:10:00 +0100

    prevents a little bug when nothing is selected

--- src/app/group-monitor/test-view/test-view.component.ts
+++ src/app/group-monitor/test-view/test-view.component.ts
@@ -81,7 +81,7 @@ export class TestViewComponent {
   }
 
   invertSelection(): boolean {
-    this.applySelection(this.selected.element, true);
+    this.applySelection(this.selected?.element, true);
     return false;
   }
 
