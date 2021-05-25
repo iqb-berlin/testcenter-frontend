371418d6868a3f97bc15d4f21500219f410bd181
paflov <paf@titelfrei.de>
2021-05-25 09:46:50 +0200

    expect version 10 of backend

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   testcenterUrl: '/api/',
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '9.1.0',
+  apiVersionExpected: '10.0.0',
   veronaApiVersionSupported: '2.1.0'
 };
