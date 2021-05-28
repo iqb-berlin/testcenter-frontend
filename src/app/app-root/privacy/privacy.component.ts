29d9b2ba94ea102fa5bdc93562a7b0d58052fcce
mechtelm <nicht@mehr.fragen>
2021-05-28 16:01:53 +0200

    Shorten impressum page

--- src/app/app-root/privacy/privacy.component.ts
+++ src/app/app-root/privacy/privacy.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MainDataService } from '../../maindata.service';
-import { AuthAccessKeyType, AuthFlagType } from '../../app.interfaces';
 
 @Component({
   templateUrl: './privacy.component.html',
@@ -9,9 +8,6 @@ import { AuthAccessKeyType, AuthFlagType } from '../../app.interfaces';
   ]
 })
 export class PrivacyComponent {
-  loginName = '';
-  loginAuthority: string[] = [];
-
   constructor(
     @Inject('APP_NAME') public appName: string,
     @Inject('APP_PUBLISHER') public appPublisher: string,
@@ -20,39 +16,4 @@ export class PrivacyComponent {
     @Inject('IS_PRODUCTION_MODE') public isProductionMode: boolean,
     public mds: MainDataService
   ) { }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      const authData = MainDataService.getAuthData();
-      if (authData) {
-        this.loginName = authData.displayName;
-        if (authData.access[AuthAccessKeyType.WORKSPACE_ADMIN]) {
-          this.loginAuthority.push('Verwaltung von Testinhalten');
-        }
-        if (authData.access[AuthAccessKeyType.SUPER_ADMIN]) {
-          this.loginAuthority.push('Verwaltung von Nutzerrechten und von grundsätzlichen Systemeinstellungen');
-        }
-        if (authData.access[AuthAccessKeyType.TEST]) {
-          if (authData.access[AuthAccessKeyType.TEST].length > 1) {
-            this.loginAuthority.push('Ausführung/Ansicht von Befragungen oder Testheften');
-          } else {
-            this.loginAuthority.push('Ausführung/Ansicht einer Befragung oder eines Testheftes');
-          }
-        }
-        if (authData.access[AuthAccessKeyType.WORKSPACE_MONITOR]) {
-          if (authData.access[AuthAccessKeyType.WORKSPACE_MONITOR].length > 1) {
-            this.loginAuthority.push('Beobachtung/Prüfung der Durchführung von Befragungen oder Kompetenztests');
-          } else {
-            this.loginAuthority.push('Beobachtung/Prüfung der Durchführung einer Befragung oder eines Kompetenztests');
-          }
-        }
-        if (authData.access[AuthAccessKeyType.TEST_GROUP_MONITOR]) {
-          this.loginAuthority.push('Beobachtung/Prüfung einer Testgruppe');
-        }
-        if (authData.flags.indexOf(AuthFlagType.CODE_REQUIRED) >= 0) {
-          this.loginAuthority.push('Code-Eingabe erforderlich');
-        }
-      }
-    });
-  }
 }
