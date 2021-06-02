5d88c90eff3cf00dd0afd72ac4c51b3d7904f34e
mechtelm <nicht@mehr.fragen>
2021-06-02 09:31:44 +0200

    Change expected Api version

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   testcenterUrl: '/api/',
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '11.0.0',
+  apiVersionExpected: '11.3.0',
   veronaApiVersionSupported: '2.1.0'
 };
