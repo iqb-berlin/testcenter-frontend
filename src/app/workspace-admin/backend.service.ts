fb60870b5ce45cd9fcd6a499ffa94421292b7e72
paf <paf@titelfrei.de>
2021-06-24 17:41:04 +0200

    Add @SkipSelf() to the injection of http-client in lazy loaded module.
    Otherwise the auth/error-handling-interceptor will not kick in.
    
    See: https://dev.to/angular/that-s-why-your-angular-errorinterceptor-may-not-work-5-seconds-fix-52m8
    
    This have to be added be added to other modules as well I guess.

--- src/app/workspace-admin/backend.service.ts
+++ src/app/workspace-admin/backend.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, Inject, SkipSelf } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -16,7 +16,7 @@ export class BackendService {
   constructor(
     @Inject('SERVER_URL') private readonly serverUrl: string,
     private wds: WorkspaceDataService,
-    private http: HttpClient
+    @SkipSelf() private http: HttpClient
   ) {
   }
 
