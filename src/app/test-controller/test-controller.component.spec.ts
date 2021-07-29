4c17a706d06483f896cd8b22b19d7e584bc04975
paflov <paf@titelfrei.de>
2021-07-29 15:44:01 +0200

    Fix test for getBookletFromXml

--- src/app/test-controller/test-controller.component.spec.ts
+++ src/app/test-controller/test-controller.component.spec.ts
@@ -21,45 +21,8 @@ import { AppError } from '../app.interfaces';
 import { TestMode } from '../config/test-mode';
 // eslint-disable-next-line import/extensions
 import { BookletConfig } from '../config/booklet-config';
-import { MaxTimerData, Testlet } from './test-controller.classes';
-
-const testBookletXML = `<Booklet>
-  <Metadata>
-    <Id>BookletId</Id>
-    <Label>Label</Label>
-  </Metadata>
-
-  <BookletConfig>
-    <Config key="force_presentation_complete">ON</Config>
-    <Config key="force_response_complete">OFF</Config>
-  </BookletConfig>
-
-  <Units>
-    <Restrictions>
-      <DenyNavigation force_presentation_complete="OFF" force_response_complete="ON"/>
-      <TimeMax minutes="10" />
-    </Restrictions>
-    <Unit id="u1" label="l" />
-    <Testlet id="t1">
-     <Restrictions>
-       <CodeToEnter code="d" />
-       <TimeMax minutes="5" />
-       <DenyNavigation force_presentation_complete="ON" force_response_complete="OFF"/>
-     </Restrictions>
-     <Unit id="u2" label="l" />
-     <Testlet id="t2">
-       <Restrictions>
-         <CodeToEnter code="d" />
-         <TimeMax minutes="3" />
-         <DenyNavigation force_presentation_complete="OFF" force_response_complete="ON"/>
-       </Restrictions>
-       <Unit id="u3" label="l" />
-     </Testlet>
-     <Unit id="u4" label="l" />
-    </Testlet>
-    <Unit id="u5" label="l" />
-  </Units>
-</Booklet>`;
+import { MaxTimerData } from './test-controller.classes';
+import { testBookletJSON, TestBookletXML } from './unit-test-example-data.spec';
 
 const testData$ = new Subject<boolean|TestData>();
 const command$ = new Subject<Command>();
@@ -98,7 +61,7 @@ const MockActivatedRoute = {
   params: routeParams$
 };
 
-fdescribe('TestControllerComponent', () => {
+describe('TestControllerComponent', () => {
   let component: TestControllerComponent;
   let fixture: ComponentFixture<TestControllerComponent>;
 
@@ -142,17 +105,9 @@ fdescribe('TestControllerComponent', () => {
   describe('getBookletFromXml', () => {
     it('should read booklet content correctly', () => {
       // eslint-disable-next-line @typescript-eslint/dot-notation
-      const booklet = component['getBookletFromXml'](testBookletXML);
-
-      const pt = (ttt: Testlet) => {
-        console.log(`========${ttt.id}` ?? ttt.sequenceId ?? 'ð');
-        console.log(ttt);
-        if (ttt.children) {
-          ttt.children.forEach(pt);
-        }
-      };
-
-      pt(booklet);
+      const booklet = component['getBookletFromXml'](TestBookletXML);
+      const bookletJSON = JSON.parse(JSON.stringify(booklet)); // strip functions
+      expect(bookletJSON).toEqual(testBookletJSON);
     });
   });
 });
---
+++ src/app/test-controller/unit-test-example-data.spec.ts
@@ -0,0 +1,172 @@
+export const TestBookletXML = `<Booklet>
+  <Metadata>
+    <Id>BookletId</Id>
+    <Label>Label</Label>
+  </Metadata>
+
+  <BookletConfig>
+    <Config key="force_presentation_complete">ON</Config>
+    <Config key="force_response_complete">OFF</Config>
+  </BookletConfig>
+
+  <Units>
+    <Restrictions>
+      <DenyNavigation force_presentation_complete="OFF" force_response_complete="ON"/>
+      <TimeMax minutes="10" />
+    </Restrictions>
+    <Unit id="u1" label="l" />
+    <Testlet id="t1">
+     <Restrictions>
+       <CodeToEnter code="d" />
+       <TimeMax minutes="5" />
+       <DenyNavigation force_presentation_complete="ON" force_response_complete="OFF"/>
+     </Restrictions>
+     <Unit id="u2" label="l" />
+     <Testlet id="t2">
+       <Restrictions>
+         <CodeToEnter code="d" />
+         <TimeMax minutes="3" />
+         <DenyNavigation force_presentation_complete="OFF" force_response_complete="ON"/>
+       </Restrictions>
+       <Unit id="u3" label="l" />
+     </Testlet>
+     <Unit id="u4" label="l" />
+    </Testlet>
+    <Unit id="u5" label="l" />
+  </Units>
+</Booklet>`;
+
+export const testBookletJSON = {
+  sequenceId: 0,
+  id: 'BookletId',
+  title: 'Label',
+  canEnter: 'y',
+  canLeave: 'y',
+  tryEnterMessage: '',
+  tryLeaveMessage: '',
+  children: [
+    {
+      sequenceId: 1,
+      id: 'u1',
+      title: 'l',
+      canEnter: 'y',
+      canLeave: 'y',
+      tryEnterMessage: '',
+      tryLeaveMessage: '',
+      children: [],
+      locked: false,
+      ignoreCompleted: false,
+      alias: 'u1',
+      naviButtonLabel: null,
+      statusResponses: 'no',
+      statusPresentation: 'no'
+    },
+    {
+      sequenceId: 0,
+      id: 't1',
+      title: '',
+      canEnter: 'y',
+      canLeave: 'y',
+      tryEnterMessage: '',
+      tryLeaveMessage: '',
+      children: [
+        {
+          sequenceId: 2,
+          id: 'u2',
+          title: 'l',
+          canEnter: 'y',
+          canLeave: 'y',
+          tryEnterMessage: '',
+          tryLeaveMessage: '',
+          children: [],
+          locked: false,
+          ignoreCompleted: false,
+          alias: 'u2',
+          naviButtonLabel: null,
+          statusResponses: 'no',
+          statusPresentation: 'no'
+        },
+        {
+          sequenceId: 0,
+          id: 't2',
+          title: '',
+          canEnter: 'y',
+          canLeave: 'y',
+          tryEnterMessage: '',
+          tryLeaveMessage: '',
+          children: [
+            {
+              sequenceId: 3,
+              id: 'u3',
+              title: 'l',
+              canEnter: 'y',
+              canLeave: 'y',
+              tryEnterMessage: '',
+              tryLeaveMessage: '',
+              children: [],
+              locked: false,
+              ignoreCompleted: false,
+              alias: 'u3',
+              naviButtonLabel: null,
+              statusResponses: 'no',
+              statusPresentation: 'no'
+            }
+          ],
+          codeToEnter: 'D',
+          codePrompt: '',
+          maxTimeLeft: 3,
+          navigationDenial: {
+            presentationComplete: false,
+            responseComplete: true
+          }
+        },
+        {
+          sequenceId: 4,
+          id: 'u4',
+          title: 'l',
+          canEnter: 'y',
+          canLeave: 'y',
+          tryEnterMessage: '',
+          tryLeaveMessage: '',
+          children: [],
+          locked: false,
+          ignoreCompleted: false,
+          alias: 'u4',
+          naviButtonLabel: null,
+          statusResponses: 'no',
+          statusPresentation: 'no'
+        }
+      ],
+      codeToEnter: 'D',
+      codePrompt: '',
+      maxTimeLeft: 5,
+      navigationDenial: {
+        presentationComplete: true,
+        responseComplete: false
+      }
+    },
+    {
+      sequenceId: 5,
+      id: 'u5',
+      title: 'l',
+      canEnter: 'y',
+      canLeave: 'y',
+      tryEnterMessage: '',
+      tryLeaveMessage: '',
+      children: [],
+      locked: false,
+      ignoreCompleted: false,
+      alias: 'u5',
+      naviButtonLabel: null,
+      statusResponses: 'no',
+      statusPresentation: 'no'
+    }
+  ],
+  codeToEnter: '',
+  codePrompt: '',
+  maxTimeLeft: 10,
+  navigationDenial: {
+    presentationComplete: false,
+    responseComplete: true
+  }
+};
