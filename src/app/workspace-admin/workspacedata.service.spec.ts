c66b19649527c7e0c0524c645001e1eb25c0f853
svwolter <sven.wolter@iqb.hu-berlin.de>
2021-08-18 11:32:00 +0200

    Add test dummies #144

--- src/app/workspace-admin/workspacedata.service.spec.ts
+++ src/app/workspace-admin/workspacedata.service.spec.ts
@@ -2,12 +2,30 @@ import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { WorkspaceDataService } from './workspacedata.service';
+import {Observable, of} from "rxjs";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+
+class MockMatDialog {
+  open(): { afterClosed: () => Observable<{ action: boolean }> } {
+    return {
+      afterClosed: () => of({ action: true })
+    };
+  }
+}
 
 describe('WorkspaceDataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      providers: [WorkspaceDataService]
+      imports: [
+        HttpClientModule,
+        MatDialogModule,
+        MatSnackBarModule
+      ],
+      providers: [
+        WorkspaceDataService,
+        { provide: MatDialog, useValue: new MockMatDialog() },
+      ]
     });
   });
 
