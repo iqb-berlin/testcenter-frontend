9c6c76d3a26f67c51671bd8756792ee4a6373671
paflov <paf@titelfrei.de>
2021-11-01 13:27:30 +0100

    Update Expected Backend Version

--- src/environments/environment.e2e.ts
+++ src/environments/environment.e2e.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   testcenterUrl: 'http://backend-mock:4010/',
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '11.3.0',
+  apiVersionExpected: '11.6.0',
   veronaApiVersionSupported: '2.1.0'
 };
